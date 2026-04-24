933a8a673653c1f568d6f936fe56864970ffb752
Jhtomaz <joaohenriquet.d@gmail.com>
2026-04-23 21:10:41 -0300

    Excluindo teste

--- src/parsear.py
+++ src/parsear.py
@@ -119,28 +119,6 @@ def parsear(tokens, tabela, simbolo_inicial):
 
 info = construirGramatica()
 
-tokens = [
-    # (START)
-    {"tipo": "LPAREN", "valor": "(", "linha": 1},
-    {"tipo": "KW_START", "valor": "START", "linha": 1},
-    {"tipo": "RPAREN", "valor": ")", "linha": 1},
-
-    # ( 3 RES )
-    {"tipo": "LPAREN", "valor": "(", "linha": 1},
-
-        {"tipo": "INT", "valor": "3", "linha": 1},
-        {"tipo": "KW_RES", "valor": "A", "linha": 1},
-
-    {"tipo": "RPAREN", "valor": ")", "linha": 1},
-
-    # (END)
-    {"tipo": "LPAREN", "valor": "(", "linha": 1},
-    {"tipo": "KW_END", "valor": "END", "linha": 1},
-    {"tipo": "RPAREN", "valor": ")", "linha": 1},
-
-    {"tipo": "$", "valor": "$", "linha": 1},
-]
-
 caminho_tokens = os.path.join(raiz, "testes", "teste1.txt")
 tokens2 = lerTokens(caminho_tokens)
 
